9f697cfe9fdc49ec687f060ed9cf7f118f9304cc
Aaron62584 <parm.aaron@gmail.com>
2024-08-30 19:58:11 -0400

    fleshed out rps with player object and reorganized code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,106 +5,143 @@ import time
 from collections import defaultdict
 from gsclasses import Player as BasicPlayer
 from gsutilities import clear_screen
+from typing import Optional
 
 class Player(BasicPlayer):
+    """custom subclass for any game-specific functionality
+    needed for the Player class."""
     def __init__(self,
                name: str,
                entity_type: str,
-               difficulty_level: int | None = None):
+               difficulty_level: Optional[int] = None):
         super().__init__(name, entity_type, difficulty_level)
-
-def get_computer_choice(options):
-    """get computer choice"""
-    # TODO:implement more complex logic for computer selection:
-    # e.g. difficulty levels, cheating, prediction methods, user-specific
-    # strategies, opponent playstyle recognition, etc.
-    return random.choice(options)
-
-def get_user_choice(options):
-    """give countdown and receive user's selection"""
-    valid_choice = False
     
-    while not valid_choice:
-        input("\nJust let me know when you're ready. (press enter)\n")
+        self.selection_raw_input = None
+        self.selection = None
 
-        for i in range(3):
-            print(f"{options[i]}...")
+    def rps_countdown(self):
+        """countdown to player choice selection using traditional
+        chant of "rock, paper, scissors" """
+        for option in options_graph.keys():
+            print(f"{option}...")
             time.sleep(0.5)
         print("\nShoot!!")
 
-        valid_choice = choice_is_valid(input().lower().strip(), options)
-    
-    return valid_choice
+    def make_selection(self):
+        """Generate/collect this player's selection from available options."""
+        if self.entity_type == "computer":
+            self.selection = self.get_choice_computer()
+        elif self.entity_type == "human":
+            try:
+                self.selection = self.get_choice_user()
+            except ValueError as e:
+                print(f"Game cancelled: {e}")
+        else:
+            raise ValueError(f"invalid entity_type for player: {self.name}")
 
-def choice_is_valid(user_choice, options):
-    """checks if user's choice is valid, and chides them if not"""
-    # check how many choices match user's selection
-    matches = [option for option in options if option.startswith(user_choice)]
+    def get_choice_computer(self):
+        """Generate a computer player's selection."""
+        # TODO: implement more complex logic for computer selection:
+        # 1. difficulty levels
+        # 2. cheating
+        # 3. prediction methods
+        # 4. user specific decision-making based on user's trends
+        # 5. selection strategies
+        return random.choice(list(options_graph.keys()))
 
-    if len(matches) == 1:
-        # if user's selection matches exactly one potential choice return true
-        return matches[0]
-    
-    else:
-        # if user's choice does not match exactly one, then chide them and
-        # return false
-        print("\nOkay, very funny. You know the options... This time,")
-        print("why don't you play right, eh? You know, by choosing... rock,")
-        print("paper, or scissors? Duh! It's in the name of the game!.")
+    def get_choice_user(self, retries=3):
+        """Collect the user's selection and parse/validate it."""
+        if retries == 0:
+            print("Too many invalid attempts. Read the instructions and try")
+            print("again sometime.")
 
-        return False
+            raise ValueError("Too many failed attempts at making a selection.")
 
-def create_graph():
-    """create directed graph"""
-    #TODO: make directed graph configurable, durable and user-specific. also
-    # implement validation logic. maybe make the graph an object with method
-    # calls to alter options and win/lose relationships.
+        input("\nJust let me know when you're ready. (press enter)\n")
+
+        self.rps_countdown()
+        self.selection_raw_input = input().lower().strip()
+        try:
+            self.parse_user_selection()
+            return self.selection
+        except ValueError as e:
+            print(f"Invalid input: {e}")
+            self.get_choice_user(retries - 1)
+                    
+    def parse_user_selection(self):
+        """Parse user's input to identify the options that match and
+        ensure that a unique selection is able to be determined."""
+        matches = [
+            option for option in options_graph.keys()
+              if option.startswith(self.selection_raw_input)]
+
+        if len(matches) == 1:
+            self.selection = matches[0]
+        elif len(matches) > 1:
+            raise ValueError("multiple options match user input")
+        else:
+            raise ValueError("no options match user input")
+
+# TODO: Make options graph custom class to handle user configuration,
+# retrieval, and storage.
+def create_options_graph():
+    """Create directed graph to represent the win/lose relationship between
+    options"""
+    # TODO: Implement validation logic.
     graph = defaultdict(list)
 
-    # Define relationships
+    # TODO: Make user configurable with persistent storage at the user level.
     graph["rock"].append("scissors")
     graph["scissors"].append("paper")
     graph["paper"].append("rock")
 
     return graph
 
-def find_winner(option_graph, p0_choice, p1_choice):
+def determine_round_winner(option_graph, players):
     """using given directed graph of options and player choices, return
     winner's index, or none if tied"""
-    if p1_choice in option_graph[p0_choice]:
+    if players[1].selection in option_graph[players[0].selection]:
         return 0
-    elif p0_choice in option_graph[p1_choice]:
+    elif players[0].selection in option_graph[players[1].selection]:
         return 1
     else:
         return None
 
-def play_round():
-    """primary game flow"""
-    # initialize the list of options in this game
-    options = [
-        "rock",
-        "paper",
-        "scissors"]
-    players = [
-        Player("You", "human"),
-        Player("I", "computer", 0)
-]
+def setup_players():
+    """Creates player objects and returns them to call procedure."""
+    return [Player("You", "human"),
+        Player("I", "computer", 0)]
 
-    # get users selection and store validated choice if possible
-    user_choice = get_user_choice(options)
-    computer_choice = get_computer_choice(options)
+def get_player_selections(players):
+    """Loops through players and initiates player selection choice for each"""
+    try:
+        for player in players:
+            player.make_selection()
+    except ValueError as e:
+        print(f"{e}")
+        return
 
-    print(f"\n{players[0].name} chose: {user_choice}")
-    print(f"{players[1].name} chose: {computer_choice}!!\n")
+def display_selections(players):
+    "Displays each player's selection after all selections have been made."
+    for player in players:
+        print(f"\n{player.name} chose: {player.selection}")
 
-    # Determine winner and respond appropriately
-    options_graph = create_graph()
-    windex = find_winner(options_graph,user_choice,computer_choice)
-    
+def resolve_round(players):
+    """Finds the winner of the round and displays the results."""
+    windex = determine_round_winner(options_graph,players)
     print("Tie!" if windex is None else f"{players[windex].name} won!!")
 
+def play_round():
+    """Runs a round of the game."""
+    players = setup_players()
+    get_player_selections(players)
+    display_selections(players)
+    resolve_round(players)
+
+options_graph = create_options_graph()
+
+"""main loop"""
 while True:
-    """main loop"""
     clear_screen()
 
     # welcome user and introduce game
